48cf2a7e147d38969ab651f7dd3d483e1d690159
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2022-05-17 20:44:36 +0900

    Add github action

--- modelgen-enhancer/build.gradle.kts
+++ modelgen-enhancer/build.gradle.kts
@@ -1,28 +1,3 @@
 plugins {
-    java
-}
-
-repositories {
-    mavenCentral()
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = Charsets.UTF_8.name()
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation(platform("org.junit:junit-bom:5.8.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+    id("jakartaee.starter.common")
 }
